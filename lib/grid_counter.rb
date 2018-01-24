5e44c538d57cb36b2eff733cefe2d6cac35b3b98
Suzanne Huldt <suzannegchuldt@gmail.com>
2018-01-24 14:43:56 +0000

    polarity and compass methods passing'

--- lib/grid_counter.rb
+++ lib/grid_counter.rb
@@ -1,23 +1,57 @@
 class GridCounter
+  attr_reader :location
 
   def initialize
     @location = [0,0]
-    @direction = 'n'
+    @facing = 'n'
   end
 
-  def get_direction
-    @direction
+  def move(direction)
+
+  end
+
+  def go_east
+    move_x('plus')
+
+  end
+
+  def go_west
+    move_x('minus')
+  end
+
+  def go_north
+    move_y('plus')
+  end
+
+  def go_south
+    move_y('minus')
+  end
+
+  def get_facing
+    @facing
   end
 
   def move_x(polarity)
-    @location = polarity(@location[0])
+    if polarity == 'minus'
+      @location[0] = minus_move(@location[0])
+    else
+      @location[0] = plus_move(@location[0])
+    end
   end
 
-  def minus(coordinate)
+  def move_y(polarity)
+    if polarity == 'minus'
+      @location[1] = minus_move(@location[1])
+    else
+      @location[1] = plus_move(@location[1])
+    end
+  end
+
+  def minus_move(coordinate)
     coordinate - 1
   end
 
-  def plus(coordinate)
+  def plus_move(coordinate)
     coordinate + 1
   end
 
--- spec/grid_counter_spec.rb
+++ spec/grid_counter_spec.rb
@@ -2,17 +2,56 @@ require './lib/grid_counter.rb'
 describe GridCounter do
   describe '#get direction' do
     it 'returns the current direction' do
-      expect(subject.get_direction).to eq ('n')
+      expect(subject.get_facing).to eq ('n')
     end
   end
   describe '#minus' do
     it 'increments input by -1' do
-      expect(subject.minus(2)).to eq(1)
+      expect(subject.minus_move(2)).to eq(1)
     end
   end
   describe '#plus' do
     it 'increments input by 1' do
-      expect(subject.plus(2)).to eq (3)
+      expect(subject.plus_move(2)).to eq (3)
     end
   end
+  describe '#move x' do
+    it 'increments the x value of location according to the polarity argument' do
+      counter = GridCounter.new
+      expect {counter.move_x('minus')}.to change {counter.location[0]}.by(-1)
+    end
+    it 'increments the x value of location according to the polarity argument' do
+      counter = GridCounter.new
+      expect {counter.move_x('plus')}.to change {counter.location[0]}.by(1)
+    end
+  end
+  describe '#go north' do
+    it 'returns the landing coordinates for one northward step' do
+      counter = GridCounter.new
+      expect {counter.go_north}.to change {counter.location[1]}.by(1)
+
+    end
+  end
+  describe '#go south' do
+    it 'returns the landing coordinates for one southward step' do
+      counter = GridCounter.new
+      expect {counter.go_south}.to change {counter.location[1]}.by(-1)
+
+    end
+  end
+  describe '#go east' do
+    it 'returns the landing coordinates for one eastward step' do
+      counter = GridCounter.new
+      expect {counter.go_east}.to change {counter.location[0]}.by(1)
+
+    end
+  end
+  describe '#go west' do
+    it 'returns the landing coordinates for one westward step' do
+      counter = GridCounter.new
+      expect {counter.go_west}.to change {counter.location[0]}.by(-1)
+
+    end
+  end
+
 end
