acb9c2c862daf23a0eb6d1fd7cdf2a21730a215b
Suzanne Huldt <suzannegchuldt@gmail.com>
2018-01-24 23:04:37 +0000

    takes json input

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,15 @@
-class Parser
+require 'json'
+require 'open-uri'
 
-  def split_input(string)
-    string.split(", ")
+class Parser
+  def get_directions
+    source = URI.parse("http://which-technical-exercise.herokuapp.com/api/suzannegchuldt@gmail.com/directions")
+    fh = source.open
+    input = fh.read
+    @json = JSON.parse(input)
+  end
+
+  def split_input
+    @directions = @json["directions"]
   end
 end
